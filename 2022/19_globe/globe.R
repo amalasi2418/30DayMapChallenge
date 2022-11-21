77050d99738769875ac0039e6ae9ed8738ac5707
amalasi2418 <abhinav.malasi@gmail.com>
2022-11-21 20:19:00 +0100

    day 21

---
+++ 2022/19_globe/globe.R
@@ -0,0 +1,45 @@
+setwd("~/R/30DayMapChallenge/2022/19_globe")
+
+library(tidyverse)
+library(sf)
+library(showtext)
+
+text = "Maven Pro"
+sysfonts::font_families_google()
+
+sysfonts::font_add_google(text,text)
+
+showtext_auto()
+
+crs <- "+proj=laea +lat_0=90 +lon_0=0 +x_0=0 +y_0=20"
+
+ports <- st_read("wld_trs_ports_wfp/wld_trs_ports_wfp.shp") %>% 
+  filter(prttype %in% c("Sea","sea","Sea Port")) %>%
+  st_transform(crs = crs)
+
+sphere_graticules <- st_graticule(ndiscr = 10000, margin = 10e-6) %>% 
+  st_transform(crs = crs) %>%
+  st_convex_hull() %>%
+  summarise(geometry = st_union(geometry))
+
+bg = "#eeeeee"
+txt_col = "#141414"
+
+plot <- ggplot() +
+  geom_sf(data = sphere_graticules, fill = txt_col, size = 0.09) + 
+  geom_sf(data = ports,size = 0.002, color = bg,alpha = 0.75)+
+  coord_sf(crs = crs) +
+  labs(title = "SEA PORTS",
+    subtitle = "Each dot is a sea port",
+    caption = "Source: OpenStreetMap | Graphic: Abhinav Malasi") +
+  theme(plot.background = element_rect(color = bg, fill = bg),
+        panel.background = element_rect(color = bg, fill = bg),
+        plot.title = element_text(hjust = 0.5, size = 150),
+        plot.subtitle = element_text(hjust = 0.5, size=60,margin = margin(t=10)),
+        plot.caption = element_text(size= 40),
+        plot.margin = margin(t=20,b=10),
+        text = element_text(color = txt_col, family = text))
+
+ggsave("sea_ports.png", plot, width=8, height = 8, dpi = 500)
+
+
---
+++ 2022/21_kontur/kontur.R
@@ -0,0 +1,48 @@
+setwd("~/R/30DayMapChallenge/2022/21_kontur")
+
+library(tidyverse)
+library(sf)
+library(showtext)
+showtext_auto()
+text="Open Sans"
+
+sysfonts::font_families_google()
+sysfonts::font_add_google(text,text)
+
+df <- st_read("kontur_population_LU_20220630.gpkg/kontur_population_LU_20220630.gpkg")
+
+txt="#EEEEEE"
+bg = "#858585"
+bg="#252525"
+
+plot <- df %>% 
+  ggplot()+
+  geom_sf(aes(fill=population),color=bg) +
+  labs(title="Grand Duchy of Luxembourg",
+       caption = "Source: Kontur Population Dataset | Graphic: Abhinav Malasi",
+       fill="Population\n(400m H3 Hexagons)") +
+  viridis::scale_fill_viridis(option = "D", 
+                              trans = "pseudo_log",  
+                              breaks = c(1,65, 650, 6500),
+                              labels = c(1,65, 650, 6500), na.value="black")+
+  guides(fill = guide_colorbar(label.position = "left", 
+                               title.hjust = 0.5,label.hjust = 1.5)) +
+  theme(plot.background = element_rect(fill=bg,color=bg),
+        panel.background = element_rect(fill=bg,color=bg),
+        panel.grid = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.title = element_text(size=100, hjust=0.5,face = "bold",margin=margin(t=20,b=10)),
+        #plot.subtitle = element_text(size=40,face = "bold",lineheight = .35),
+        plot.caption = element_text(size=30,margin = margin(b=10,r=20)),
+        plot.margin = margin(t=20,b=10),
+        legend.title = element_text(size=40,lineheight=.25),
+        legend.position = c(.95,.8),
+        legend.key.width = unit(0.5, "line"),
+        legend.text = element_text(size=35),
+        legend.background = element_rect(fill=NA,color=NA),
+        legend.key = element_rect(fill=bg,color=bg),
+        text=element_text(family=text,color=txt))
+
+
+ggsave("luxembourg.png",plot,width = 8,height = 8,units = "in",dpi=320)
